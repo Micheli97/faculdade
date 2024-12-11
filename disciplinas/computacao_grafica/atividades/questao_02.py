e7992acfbd08a9f18815abee77b5094669b28bbb
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2024-12-11 18:06:39 -0300

    refactor(): questao 02

--- disciplinas/computacao_grafica/atividades/questao_02.py
+++ disciplinas/computacao_grafica/atividades/questao_02.py
@@ -2,31 +2,36 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import * 
 from OpenGL.GLU import *
-import sys
 
-# Lista de vértices ajustada para 3 dimensões
+
+
 vertices = [
-    [-0.5, 0.5, 0], # A
-    [-0.5, -0.5, 0], # B
-    [0.5, -0.5, 0], # C
-    [0.5, 0.5, 0], # D   
+    [-0.5, 0.5, 0],  # Vertice superior esquerdo
+    [-0.5, -0.5, 0], # Vertice infeior esquerdo
+    [0.5, -0.5, 0],  # Vertice inferior direito
+    [0.5, 0.5, 0],   # Vertice superior direito   
 ]
 
 vertices_quadrado_central = [
-    [-0.02, 0.02, 0], # A
-    [-0.02, -0.02, 0], # B
-    [0.02, -0.02, 0], # C
-    [0.02, 0.02, 0], # D   
+    [-0.02, 0.02, 0],  # Vertice superior esquerdo
+    [-0.02, -0.02, 0], # Vertice infeior esquerdo
+    [0.02, -0.02, 0],  # Vertice inferior direito
+    [0.02, 0.02, 0],   # Vertice superior direito   
 ]
 
 
+def desenha_quadrado(vertice_lista, tipo_primitiva, color):
+    
+    glColor3f(*color)
 
-def renderiza():
-    glClear(GL_COLOR_BUFFER_BIT)
+    glBegin(tipo_primitiva)
+    for vertice in vertice_lista:
+        glVertex3fv(vertice)
+    glEnd()
 
-    glPointSize(10)
-    glLineWidth(10)
-    glColor3f(1, 0, 0)
+
+def desenha_cruz():
+    glColor3f(1,0,0)
 
     # Linha vertical da cruz
     glBegin(GL_LINES)
@@ -34,45 +39,33 @@ def renderiza():
     glVertex2f(0, 1)
     glEnd()
    
-    # Linha horizontal
+    # # Linha horizontal
     glBegin(GL_LINES)
     glVertex2f(-1, 0)
     glVertex2f(1, 0)
     glEnd()
 
+
+def renderiza():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glPointSize(10)
+    glLineWidth(10)
+
+    desenha_cruz()
+   
     # Quadrado central
-    glBegin(GL_POINTS)
-    for v in vertices:
-        glVertex3fv(v)  
-    glEnd()
+    desenha_quadrado(vertice_lista=vertices,tipo_primitiva= GL_POINTS, color=(1,0,0))
+    desenha_quadrado(vertice_lista=vertices,tipo_primitiva= GL_LINE_LOOP, color=(1,0,0))
 
-    # Ligando os vertices
-    glBegin(GL_LINE_LOOP)
-    for v in vertices:
-        glVertex3fv(v)  
-    glEnd()
-
-    
     # Quadrado central
-    glColor3f(255, 255, 0)
-    glBegin(GL_POINTS)
-    for v in vertices_quadrado_central:
-        glVertex3fv(v)  
-    glEnd()
-
-    # Ligando vertices do quadrado central
-    glBegin(GL_LINE_LOOP)
-    for v in vertices_quadrado_central:
-        glVertex3fv(v)  
-    glEnd()
-
+    desenha_quadrado(vertice_lista=vertices_quadrado_central,tipo_primitiva= GL_POINTS, color=(255, 255, 0))
+    desenha_quadrado(vertice_lista=vertices_quadrado_central,tipo_primitiva= GL_LINE_LOOP, color=(255, 255, 0))
 
 
 
 def main():
     pygame.init()
-
-   
 
     display = (400, 400)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
@@ -93,55 +86,3 @@ def main():
 
 main()
 
-# class App:
-#     def __init__(self):
-#         pygame.init()
-#         pygame.display.set_mode((640, 480), pygame.OPENGL | pygame.DOUBLEBUF)
-#         self.clock = pygame.time.Clock()
-#         glClearColor(1, 1, 1, 1) #background
-#         self.input = Input()
-
-#     def initialize(self):
-#         pass
-
-#     def update(self):
-#         pass 
-
-
-#     def run(self):
-#         self.initialize()
-#         while True:
-#            self.input.update()
-#            if self.input.quit:
-#                break 
-#            self.update()
-#            pygame.display.flip()
-#            self.clock.tick(60)
-#         pygame.quit()
-#         sys.exit()
-
-
-# class Input():
-#     def __init__(self):
-#         self.quit = False 
-
-#     def update(self):
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 self.quit = True
-
-# class Forma:
-#     def __init__(self, color):
-#         self.color = color 
-
-#     def draw(self):
-#         pass
-
-# class Desenho(App):
-#     def initialize(self):
-#         self.input
-#         return super().initialize()
-
-
-# if __name__ == "__main__":
-#     myapp = App()
